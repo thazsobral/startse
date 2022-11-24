fd1783816791f6d2c8b6e8b2a81e89a2a1aa48a5
thazsobral <thazsobral@gmail.com>
2022-11-24 07:08:07 -0300

    feature: add show or hidden check

--- pratice/tasks-list/index.js
+++ pratice/tasks-list/index.js
@@ -1,6 +1,7 @@
 const newTask = document.getElementById("new-task");
 const newTaskButton = document.getElementById("new-task-button");
 const tasksList = document.getElementById("tasks-list");
+const selectAllTasks = document.getElementById("select-tasks");
 
 newTaskButton.addEventListener("click", function() {
     let task = newTask.value;
@@ -17,14 +18,34 @@ newTaskButton.addEventListener("click", function() {
     liElement.appendChild(buttonElement);
 
     tasksList.appendChild(liElement);
+
+    showOrHiddenSelectTasks();
 });
 
 function removeElement(e) {
     e.preventDefault();
     e.target.parentNode.remove();
+
+    showOrHiddenSelectTasks();
 };
 
 function toggleStatus(e) {
     e.preventDefault();
     e.target.className = e.target.className === "" ? "deactive" : "";
 }
+
+function showOrHiddenSelectTasks() {
+    let containerElement = document.getElementById("select-tasks-container");
+    let tasks = document.querySelector("span");
+    if(tasks === null) {
+        containerElement.setAttribute("hidden", "hidden");
+    } else {
+        containerElement.removeAttribute("hidden");
+    }
+}
+
+selectAllTasks.addEventListener("change", function(e) {
+    e.preventDefault();
+    console.log(e);
+    return;
+});
